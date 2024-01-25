72398147e569873175256c1d0ec3d50fc639d329
9alexx3 <58992048+9alexx3@users.noreply.github.com>
2024-01-26 00:28:50 +0100

    PRE-ALPHA
    Pre-Alpha version

---
+++ js/easter-eggs.js
@@ -0,0 +1,46 @@
+//! EASTER EGG Nº 1 //
+
+const emoji = `
+           ██
+  ████    ██
+  ████   ██  
+         ██  
+  ████   ██  
+  ████    ██
+           ██
+  
+Your PC ran into a problem and needs to restart. We're just collecting some error info, and then we'll  restart for you.
+
+For more information about this issue and possibles fixes, contact Alejandro C.
+`
+ 
+const qr = `
+▄▄▄▄▄▄▄   ▄ ▄▄▄▄  ▄▄▄▄▄▄  ▄▄▄▄▄▄▄
+█ ▄▄▄ █ ▀ ▀ ▄█▀█▀   ▄▄    █ ▄▄▄ █
+█ ███ █ ▀█▀ ▀ ▀█▄▀ ▄█▄ ▄▀ █ ███ █
+█▄▄▄▄▄█ █▀▄▀█ ▄ ▄▀█ █▀▄ █ █▄▄▄▄▄█
+▄▄▄▄▄ ▄▄▄█▀█  ▀ █▄█▀▄▀▄▀ ▄ ▄ ▄ ▄ 
+▀ ▀▀▄ ▄ █ █ ▀███▄ █▀███▀  ▀▀▄ ▀█▀
+▀  █▀▀▄▀▀▄▄ █▀▄█ ▄▀  ▀▄█▄▄ ▀▄▀▀█▀
+ ▀██  ▄█▄ ▄▄▄  ▀▀ ▀▀█▄ ▀ ▄▀▄▄▄█▄▀
+█▀  ▀▀▄█▄▀▀█  ▀ █▄  ▀▀▄▀█▄▀▄██▀▄█
+ ██▄▀▄▄▄▀▀█ ▀███▄▄▀▀▀█▄▀ ▄█ ▄▀█▄▀
+▄ █▀ ▀▄█ ▄▀ █▀▄█ ▄   ▀▀▀▀█▀▄██▀██
+█▀▀▄█▄▄ ▀▀█▄▄  ▀▀▄▀▀ █ ▀ ▀▀  ██▄▀
+█ ▄▀▀ ▄██▄ █  ▀ ▀▄▄ █▀▄▄█▄███▀▀▀▀
+▄▄▄▄▄▄▄ █▄  ▀███▄ █▀█▀▄▄█ ▄ █▀▄▄ 
+█ ▄▄▄ █ ▄▄▀ █▀▄█ ▄▄▀▄▀▄▀█▄▄▄█▀▀█ 
+█ ███ █ █ █▄▄  ▄▄ █▀▀▄ ▄█ ▄▀▀▄█▀▀
+█▄▄▄▄▄█ ██▄█  ▀█ ▄▄▀▄▀█▄▄  ▀ █▀▄ 
+
+
+\x1b[33m  Use white theme to scan the QR code. ${colors.reset}`
+console.log(emoji)
+console.log(qr)
+
+
+
+
+//! EASTER EGG Nº 2 //
+
+// Lo de arriba, abajo, arriba, etc.
